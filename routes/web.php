fbb38b8d0b15a57c09636d69d7c6098e59e96f62
josh <joshmackinnon8@gmail.com>
2026-02-07 20:09:45 -0400

    user dashboard and nav bar with placeholders

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Livewire\MyProgress;
+use App\Livewire\UserFormSelect;
+use App\Livewire\UserTodo;
 
 Route::get('/', function () {
     return view('welcome');
@@ -14,4 +17,10 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+    Route::get('/my-progress', MyProgress::class)
+        ->name('my-progress');
+    Route::get('/user-form-select', UserFormSelect::class)
+        ->name('user-form-select');
+    Route::get('/user-todo', UserTodo::class)
+        ->name('user-todo');
 });
