bb78678312932b1a88b5351923f1b5da4916cf13
josh <joshmackinnon8@gmail.com>
2026-02-09 21:46:03 -0400

    add name and slug fields to FormTemplate model + migration, add laravel-sluggable package

--- app/Models/FormTemplate.php
+++ app/Models/FormTemplate.php
@@ -4,14 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use App\Traits\HasUuid;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 
 class FormTemplate extends Model
 {
     use HasUuid;
+    use HasSlug;
 
     public $timestamps = false;
 
     protected $fillable = [
+        'name',
+        'slug',
         'version',
         'status',
         'description',
@@ -25,5 +30,18 @@ class FormTemplate extends Model
     public function submissions()
     {
         return $this->hasMany(FormSubmission::class);
+    }
+
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('name')
+            ->saveSlugsTo('slug')
+            ->doNotGenerateSlugsOnUpdate();
+    }
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
     }
 }
