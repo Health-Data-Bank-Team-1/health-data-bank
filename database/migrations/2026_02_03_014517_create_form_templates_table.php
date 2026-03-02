4dd33e6ed5c3d0f3448397e213032a0c2c0f1c46
josh <joshmackinnon8@gmail.com>
2026-03-02 17:13:27 -0400

    fix accidental delete

--- database/migrations/2026_02_03_014517_create_form_templates_table.php
+++ database/migrations/2026_02_03_014517_create_form_templates_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('slug')->unique();
             $table->text('description')->nullable();
             $table->integer('version')->default(1);
+            $table->timestamp('created_at')->useCurrent();
         });
     }
 
