791a046f89af3d35f02b7109bfa601ab951b069d
josh <joshmackinnon8@gmail.com>
2026-03-07 14:37:32 -0400

    add dashboards, layouts, nav bars for researcher and admin, placeholders for the elements, pages restricted with role based middleware

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Role;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
