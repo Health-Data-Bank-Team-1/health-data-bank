89093620f8965aca27ba9cb7bc1787a0c6dfa69e
josh <joshmackinnon8@gmail.com>
2026-03-10 02:37:20 -0300

    cohort builder ui

--- app/Livewire/Researcher/ResearcherReportGenerator.php
+++ app/Livewire/Researcher/ResearcherReportGenerator.php
@@ -6,6 +6,31 @@ use Livewire\Component;
 
 class ResearcherReportGenerator extends Component
 {
+    public $filters = [];
+
+    public function mount()
+    {
+        $this->filters = [
+            ['field' => '', 'operator' => '', 'value' => '']
+        ];
+    }
+
+    public function addFilter()
+    {
+        $this->filters[] = ['field' => '', 'operator' => '', 'value' => ''];
+    }
+
+    public function removeFilter($index)
+    {
+        unset($this->filters[$index]);
+        $this->filters = array_values($this->filters);
+    }
+
+    public function generateCohort()
+    {
+        $this->dispatch('cohortGenerated', filters: $this->filters);
+    }
+
     public function render()
     {
         return view('livewire.researcher.report-generator')
